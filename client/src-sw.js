426e7a79a8aa55345b8e084db6ed393bef381491
Frank Lavery <frank.lavery@westnet.com.au>
2022-07-10 13:17:37 +0800

    progress commit

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -24,20 +24,18 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => {
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-    new StaleWhileRevalidate({
-      // Name of the cache storage.
-      cacheName: 'page-cache',
-      plugins: [
-        // This plugin will cache responses with these headers to a maximum-age of 30 days
-        new CacheableResponsePlugin({
-          statuses: [0, 200],
-        }),
-      ],
-    });
-  request.mode === 'navigate', pageCache
-});
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implement asset caching
+
+registerRoute(
+  ({ request }) => ['style', 'script', 'document'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,18 +12,19 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('write to the database');
+  console.log('Write to the database');
   const jateDb = await openDB('jate', 1);
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ content: content });
+  const request = store.put({content: content });
   const result = await request;
   console.log('data written to the database', result);
+  return result;
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET operation');
   const jateDb = await openDB('jate', 1);
